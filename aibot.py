fb773682bb30e6d4f348d841057f7dba7ce2bb89
Adarsh Kumar <123639529+adarshkumar714@users.noreply.github.com>
2023-07-14 13:13:26 +0530

    aibot

---
+++ aibot.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# author: Adarsh Kumar (https://github.com/adarshkumar714)
+
+# utilites / helper functions
+
+from langchain.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+import pinecone
+from langchain.vectorstores import Pinecone
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.llms import OpenAI
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+
+
+# environment variables
+import os  
+from dotenv import load_dotenv
+load_dotenv()
+
+documents = []
+url = "https://www.ideou.com/blogs/inspiration/what-is-design-thinking"
+
+
+# loading documents
+print(f"[+] loading data from URL:{url}")
+loader = WebBaseLoader(url)
+doc = loader.load()
+documents.extend(doc)
+print("[+] URL loaded")
+
+
+# splitting into chunks
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=512, chunk_overlap=10)
+documents = text_splitter.split_documents(documents)
+
+
+# embeddings
+embeddings = OpenAIEmbeddings(model='gpt-3.5-turbo', openai_api_key=os.getenv('OPENAI_API_KEY'))
+
+
+# initializing pinecone db
+print('[+] Initializing pinecone db...')
+pinecone.init(
+    api_key=os.getenv('PINECONE_API_KEY'),
+    environment=os.getenv('PINECONE_ENV'),
+)
+
+index_name = os.getenv('PINECONE_INDEX_NAME')
+index = pinecone.Index(index_name)
+index.describe_index_stats()
+
+print("[+] Index Stats: ")
+print(index.describe_index_stats())
+docsearch = Pinecone.from_documents(documents, embedding=embeddings, index_name=index_name)
+
+print('[+] Creating vector store')
+text_field = "text"
+
+# switch back to normal index for langchain
+index = pinecone.Index(index_name)
+
+# embed will be created by client
+vectorstore = Pinecone(index, embeddings.embed_query, text_field)
+
+
+# Chat
+
+# completion llm
+llm = ChatOpenAI(openai_api_key=os.getenv('OPENAI_API_KEY'),
+                 model_name='gpt-3.5-turbo',
+                 temperature=0.0)
+
+# qa = RetrievalQA.from_chain_type(
+#     llm=llm,
+#     chain_type="stuff",
+#     retriever=vectorstore.as_retriever()
+# )
+
+# memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+qa = ConversationalRetrievalChain.from_llm(
+    llm = llm,
+    retriever = vectorstore.as_retriever()
+)
+
+
+# returns result of the query and the updated chat_history
+def get_response(query, chat_history):
+    result = qa({"question": query, "chat_history": chat_history})["answer"]
+    chat_history.append((question, result["answer"]))
+
+    return result, chat_history
+
+
--- bot.py
+++ bot.py
@@ -1,71 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# author: Adarsh Kumar (https://github.com/adarshkumar714)
+
 """
 ### discord bot url ###
 https://discord.com/api/oauth2/authorize?client_id=1128957735157899274&permissions=326417737728&scope=bot
 """
 
-
-#!/usr/bin/env python3
-
-# author: Adarsh Kumar (https://github.com/adarshkumar714)
+from aibot import get_response
 
 import discord
-import openai
-import requests
-import json
-import os
-
-openai.api_key = os.getenv('OPENAI_KEY')
-
-
-def get_quote():
-    try:
-        response = requests.get("https://zenquotes.io/api/random")
-        json_data = json.loads(response.text)
-        quote = json_data[0]['q'] + ' -' + json_data[0]['a']
-        return quote
-    except:
-        print("[!] random quotes api is not working, need to fix it")
-        return "SORRY NO QUOTE AVAILABLE !"
-
-def get_joke():
-    try:
-        joke = requests.get('https://jokes-api.gamhcrew.repl.co/')
-        return joke.text
-    except:
-        print("[!] random joke api is not working, need to fix it")
-        return "SORRY NO JOKE AVAILABLE !"
-
-def chat_gpt_get(content):
-    try:
-        response = openai.ChatCompletion.create(
-            model = "gpt-3.5-turbo",
-            messages = [ 
-                {"role": "user", "content": content}
-            ]
-        )["choices"][0]["message"]["content"] 
-        return response
-    except Exception as e:
-        print("[!] Failed to get Chat-GPT reponse, need to fix it")
-        print(e)
-        return "SORRY SOME INTERNAL ERROR !"
-
-
-
-
-
-
-
-
-
-
 from discord.ext import commands
+import requests
 import os
 
 from dotenv import load_dotenv
-
 load_dotenv()
 
 client = commands.Bot(command_prefix="$", intents=discord.Intents.all())
+
+
+"""
+for storing chat histories for individual chats:
+
+key -> unique chat id
+value -> chat history for that chat
+"""
+history = dict() 
 
 
 @client.event
@@ -78,5 +39,6 @@ async def on_ready():
 async def on_message(message):
     print(message)
     # ...
+
 
 client.run('a446c58ebb655611de56b2a2162b83e3e1cfd4f09efc27c5b2ae83f8e7331514')
