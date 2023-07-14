3e79a45a819f301af01f7398ae102d5fc19402da
Adarsh Kumar <123639529+adarshkumar714@users.noreply.github.com>
2023-07-14 22:54:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,71 +1,100 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # author: Adarsh Kumar (https://github.com/adarshkumar714)
-"""
-### discord bot url ###
-https://discord.com/api/oauth2/authorize?client_id=1128957735157899274&permissions=326417737728&scope=bot
-"""
 
-# from aibot import get_response
+from langchain.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+import pinecone
+from langchain.vectorstores import Pinecone
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.llms import OpenAI
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
 
-import discord
-from discord.ext import commands
-from discord.utils import find
-import requests
+# environment variables
 import os
-
 from dotenv import load_dotenv
+
+os.environ['OPENAI_API_KEY'] = os.getenv(
+  "OPENAI_API_KEY")  # repl.it asks for this useless line
 
 load_dotenv()
 
-bot = commands.Bot(command_prefix="$", intents=discord.Intents.all())
-"""
-for storing chat histories for individual chats:
+documents = []
+url = "https://www.ideou.com/blogs/inspiration/what-is-design-thinking"
 
-key -> unique chat id
-value -> chat history for that chat
-"""
-history = dict()
+# loading documents
+print(f"[+] loading data from URL:{url}")
+loader = WebBaseLoader(url)
+doc = loader.load()
+documents.extend(doc)
+print("[+] URL loaded")
+
+# splitting into chunks
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=512,
+                                               chunk_overlap=10)
+documents = text_splitter.split_documents(documents)
+
+# embeddings
+embeddings = OpenAIEmbeddings()
+
+# initializing pinecone db
+print('[+] Initializing pinecone db...')
+pinecone.init(
+  api_key=os.getenv('PINECONE_API_KEY'),
+  environment=os.getenv('PINECONE_ENV'),
+)
+
+index_name = os.getenv('PINECONE_INDEX_NAME')
+index = pinecone.Index(index_name)
+index.describe_index_stats()
+
+print("[+] Index Stats: ")
+print(index.describe_index_stats())
+docsearch = Pinecone.from_documents(documents,
+                                    embedding=embeddings,
+                                    index_name=index_name)
+
+print('[+] Creating vector store')
+text_field = "text"
+
+# switch back to normal index for langchain
+index = pinecone.Index(index_name)
+
+# embed will be created by client
+vectorstore = Pinecone(index, embeddings.embed_query, text_field)
+
+# Chat
+
+# completion llm
+llm = ChatOpenAI(openai_api_key=os.getenv('OPENAI_API_KEY'),
+                 model_name='gpt-3.5-turbo',
+                 temperature=0.0)
+
+# qa = RetrievalQA.from_chain_type(
+#     llm=llm,
+#     chain_type="stuff",
+#     retriever=vectorstore.as_retriever()
+# )
+
+# memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+qa = ConversationalRetrievalChain.from_llm(
+  llm=llm, retriever=vectorstore.as_retriever())
 
 
-def chat_history(user_id):
-    global history
-    try:
-        return history[user_id]
-    except:
-        history[user_id] = []
-        return history[user_id]
+# returns result of the query and the updated chat_history
+def get_response(query, chat_history=[]):
+  result = qa({"question": query, "chat_history": chat_history})["answer"]
+  chat_history.append((query, result))
 
-
-@bot.event
-async def on_ready():
-    print('we have logged in as %s' % bot.user)
-
-@bot.event
-async def on_guild_join(guild):
-    general = find(lambda x: x.name == 'general',  guild.text_channels)
-    if general and general.permissions_for(guild.me).send_messages:
-        await general.send('Hello {}!'.format(guild.name))
-
-@bot.event
-async def on_message(message):
-    print(str(message.type))
-    print('second', str(message.type)=='MessageType.new_member')
-    if message.author!=bot.user and str(message.type) != 'MessageType.new_member':
-        user_id = message.author
-        query = message.content
-        print('this is message after checking if a new user joins the server')
-        # print(message)
-    
-        await message.channel.send('generating...')
-        # response, history[user_id] = get_response(query, chat_history(user_id))
-        response = f'this is the response {user_id}'
-        await message.channel.send(response, reference=message)
-
-
-def run_bot():
-    bot.run(os.environ['DISCORD_BOT_TOKEN'])
+  return result, chat_history
 
 
 if __name__ == "__main__":
-    run_bot()
+  print("START THE CHAT:\n")
+  while True:
+    query = input("[You]: ")
+    response, chat_history = get_response(query, [])
+    print(response)
