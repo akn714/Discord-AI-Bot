e31b3442f3cc2fe3c475acbd3f8badaee68c9f2f
Adarsh Kumar <123639529+adarshkumar714@users.noreply.github.com>
2023-07-14 15:05:59 +0530

    bot

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # author: Adarsh Kumar (https://github.com/adarshkumar714)
-
 """
 ### discord bot url ###
 https://discord.com/api/oauth2/authorize?client_id=1128957735157899274&permissions=326417737728&scope=bot
@@ -15,30 +14,35 @@ import requests
 import os
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
 client = commands.Bot(command_prefix="$", intents=discord.Intents.all())
-
-
 """
 for storing chat histories for individual chats:
 
 key -> unique chat id
 value -> chat history for that chat
 """
-history = dict() 
+history = dict()
 
 
 @client.event
 async def on_ready():
-    print('we have logged in as %s'%client.user)
-    # ...
+  print('we have logged in as %s' % client.user)
+  # ...
 
 
 @client.event
 async def on_message(message):
-    print(message)
-    # ...
+  print(message)
+  # ...
 
 
-client.run('a446c58ebb655611de56b2a2162b83e3e1cfd4f09efc27c5b2ae83f8e7331514')
+def run_bot():
+  client.run(
+      'a446c58ebb655611de56b2a2162b83e3e1cfd4f09efc27c5b2ae83f8e7331514')
+
+
+if __name__ == "__main__":
+  run_bot()
