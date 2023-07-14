32cb8c3c315508c0664bb474f2bd6d646be7a39b
Adarsh Kumar <123639529+adarshkumar714@users.noreply.github.com>
2023-07-14 19:36:04 +0530

    updated on_message and added on_guild_join

--- bot.py
+++ bot.py
@@ -6,10 +6,11 @@
 https://discord.com/api/oauth2/authorize?client_id=1128957735157899274&permissions=326417737728&scope=bot
 """
 
-from aibot import get_response
+# from aibot import get_response
 
 import discord
 from discord.ext import commands
+from discord.utils import find
 import requests
 import os
 
@@ -17,7 +18,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client = commands.Bot(command_prefix="$", intents=discord.Intents.all())
+bot = commands.Bot(command_prefix="$", intents=discord.Intents.all())
 """
 for storing chat histories for individual chats:
 
@@ -36,25 +37,34 @@ def chat_history(user_id):
         return history[user_id]
 
 
-@client.event
+@bot.event
 async def on_ready():
-    print('we have logged in as %s' % client.user)
+    print('we have logged in as %s' % bot.user)
 
+@bot.event
+async def on_guild_join(guild):
+    general = find(lambda x: x.name == 'general',  guild.text_channels)
+    if general and general.permissions_for(guild.me).send_messages:
+        await general.send('Hello {}!'.format(guild.name))
 
-@client.event
+@bot.event
 async def on_message(message):
-    user_id = message.author
-    query = message.content
-    print(message)
-
-    await message.channel.send('generating...')
-    response, history[user_id] = get_response(query,
-                                                   chat_history(user_id))
-    await message.channel.send(response, reference=message)
+    print(str(message.type))
+    print('second', str(message.type)=='MessageType.new_member')
+    if message.author!=bot.user and str(message.type) != 'MessageType.new_member':
+        user_id = message.author
+        query = message.content
+        print('this is message after checking if a new user joins the server')
+        # print(message)
+    
+        await message.channel.send('generating...')
+        # response, history[user_id] = get_response(query, chat_history(user_id))
+        response = f'this is the response {user_id}'
+        await message.channel.send(response, reference=message)
 
 
 def run_bot():
-    client.run(os.getenv('DISCORD_BOT_TOKEN'))
+    bot.run(os.environ['DISCORD_BOT_TOKEN'])
 
 
 if __name__ == "__main__":
